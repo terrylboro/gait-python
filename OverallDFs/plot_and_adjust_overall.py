d1955f379c8f5d49988fba3a78aadef0dea2c4bf
Terry <terryfawden@gmail.com>
2024-06-06 15:16:37 +0100

    plotting fp vs wrist acceleration to explore possible sync

--- OverallDFs/plot_and_adjust_overall.py
+++ OverallDFs/plot_and_adjust_overall.py
@@ -9,22 +9,28 @@ activityDF = pd.read_csv('../C3d/ActivitiesIndex.csv')
 
 
 # plot a walking trial
-for subjectNum in [x for x in range(66, 67) if x not in [11, 47, 48, 49]]:
+for subjectNum in [x for x in range(60, 68) if x not in [11, 20, 22, 24, 47, 48, 49]]:
     dataDir = "TF_{}/".format(str(subjectNum).zfill(2))
     print(dataDir)
     for file in os.listdir(dataDir):
         df = pd.read_csv(os.path.join(dataDir, file))
         trialNum = int(file.split("-")[-1].split(".")[0])
-        # g = sns.lineplot(data=data[data["Subject"].isin(range(1, 68))], x='Subject', y=metric, hue='Condition',
-        #             hue_order=["Typical", "Parkinsons", "Comorbities", "Balance"])
-        sns.lineplot(data=df[["AccZLeft", "AccYWrist"]])
-        fpNonZero = df["FP1Z"][df["FP1Z"] != 0]
-        fpNonZero = fpNonZero[fpNonZero.notna()].abs()
-        # print(fpNonZero)
-        # print(fpNonZero.max())
-        fpNonZeroNormalised = fpNonZero.div(fpNonZero.max()) * 10
-        # print(fpNonZeroNormalised)
-        # print(df["FP1Z"].div(max(df["FP1Z"].to_numpy()), fill_value=0).mul(10, fill_value=0))
-        sns.lineplot(data=fpNonZeroNormalised)
-        plt.vlines(fpNonZeroNormalised.index[0], 0, 10, linestyles='solid', colors='r')
-        plt.show()
+        if trialNum == 4:
+            sns.lineplot(data=df[["AccZLeft", "AccYWrist"]])
+            wristResAcc = np.sqrt(np.sum(np.square(df[["AccXWrist", "AccYWrist", "AccZWrist"]]), axis=1))
+            # print(wristResAcc[wristResAcc != 0])
+            sns.lineplot(data=wristResAcc)
+            # sns.lineplot(data=df["AccYWrist"])
+            fp1NonZero = df["FP1Z"][df["FP1Z"] < 0]
+            fp1NonZero = fp1NonZero[fp1NonZero.notna()].abs()
+            fp2NonZero = df["FP2Z"][df["FP2Z"] < 0]
+            fp2NonZero = fp2NonZero[fp2NonZero.notna()].abs()
+            fp1NonZeroNormalised = fp1NonZero.div(fp1NonZero.max()) * 10
+            fp2NonZeroNormalised = fp2NonZero.div(fp2NonZero.max()) * 10
+            # print(fpNonZeroNormalised)
+            # print(df["FP1Z"].div(max(df["FP1Z"].to_numpy()), fill_value=0).mul(10, fill_value=0))
+            sns.lineplot(data=fp1NonZeroNormalised)
+            sns.lineplot(data=fp2NonZeroNormalised)
+            plt.vlines([fp1NonZeroNormalised.index[0], fp2NonZeroNormalised.index[0]], 0, 10, linestyles='solid', colors='r')
+            plt.title("{}-{}".format(subjectNum, trialNum))
+            plt.show()
